9475d8b2d6dedb294c6fda3c964162331ff581d8
alaedine07 <alaedineboufaden@gmail.com>
2022-01-08 22:18:41 +0100

    create blogusers with User Profile method

--- usermanagment/admin.py
+++ usermanagment/admin.py
@@ -1,1 +1,8 @@
+from django.contrib import admin
+from usermanagment.models import BlogUsers
 
+class BlogUsersAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(BlogUsers, BlogUsersAdmin)
---
+++ usermanagment/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class BlogUsers(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    facebook = models.CharField(max_length=300, blank=True, null=True)
+    instagram = models.CharField(max_length=300, blank=True, null=True)
+    linkedin = models.CharField(max_length=300, blank=True, null=True)
+    github = models.CharField(max_length=300, blank=True, null=True)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return str(self.user)
