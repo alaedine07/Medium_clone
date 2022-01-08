5bcd792a308fb65bb11f3233127d52645ab79a25
alaedine07 <alaedineboufaden@gmail.com>
2022-01-08 20:54:14 +0100

    delete previous migrations

--- usermanagment/views.py
+++ usermanagment/views.py
@@ -3,17 +3,6 @@ from django.contrib import messages
 # this uses premade forms from django
 # from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
-def login_page(request):
-    """ render the login page to the user """
-    if request.method == "GET":
-        return render(request, "login.html")
-    elif request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        # print(username, password)
-    return render(request, "login.html")
-
 def register_page(request):
     """ render the registration page """
     if request.method == "GET":
